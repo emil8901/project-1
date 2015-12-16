1c9657d4ae052284f3b846a6cb42e6f13b26157f
emil8901 <emil.iwicki@gmail.com>
2015-12-16 22:00:55 +0100

    Create reminder.h

---
+++ reminder.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void reminder()
+{
+
+	cout << "Instrukcja użytkownia programu:\n\n"
+		"- przesuwanie figury odbywa się przy pomocy strzałek\n"
+		"- zmienianie rozmiarów figury odbywa się przy pomocy klawiszy + i -\n"   // przypomnienie obsługi programu dla użytkownika
+		"- zakończenie programu = Esc\n\n\n\n\n"
+		"- Naciśnij dowolny klawisz, aby rozpocząć działaie programu...";
+
+	getch();   
+
+	system("cls");
+}

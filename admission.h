0fca651d3320b981301d394207af67ec6dc4fb62
emil8901 <emil.iwicki@gmail.com>
2015-12-16 22:00:40 +0100

    Create admission.h

---
+++ admission.h
@@ -0,0 +1,31 @@
+#pragma once 
+
+void admission()
+{
+
+	cout << "****************************************************************************************************\n"    // opis programu dla użytkownika
+		"Witaj w programie rysowania poniższej figury za pomocą wybranego znaku ASCII\n\n"
+		"                                               .\n"
+		"                                             .   .\n"
+		"                                           .       .\n"
+		"                                         .           .\n"
+		"                                       .               .\n"
+		"                                     . . .           . . .\n"
+		"                                         .           .\n"
+		"                                         .           .\n"
+		"                                         .           .\n"
+		"                                         .           .\n"
+		"                                         . . . . . . .\n\n"
+		"     Program umożlwia:\n\n"
+		"     - wybranie znaku kodu ASCII\n"
+		"     - wczytanie początkowych rozmiarów figury\n"
+		"     - przesuwanie figury strzałkami\n"
+		"     - powiększenie oraz zmniejszenie rozmiaru figury za pomocą klawiszy + i -\n\n"
+		"****************************************************************************************************\n\n"
+		"Naciśnij dowolny klawisz...";
+
+	getch();
+
+	system("cls");
+	
+}
